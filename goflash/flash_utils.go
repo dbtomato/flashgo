c6635fe22140b90063bab4b4cc48ad5f16b05726
maxt <maxt@ziroom.com>
2020-04-04 11:45:18 +0800

    commit bin

---
+++ goflash/flash_utils.go
@@ -0,0 +1,345 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/siddontang/go-mysql/replication"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func isValidDatetime(dateStr string) bool {
+	_, err := time.Parse("2006-01-02 15:04:05", dateStr)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//判断文件是否存在
+func fileExist(path string) bool {
+	//使用Lstat比使用open效率更高
+	_, err := os.Lstat(path)
+	return !os.IsNotExist(err)
+}
+
+func createUniqueFile(filename string) (resultFileRes string, err error) {
+	resultFile := filename
+	//if we have to try more than 1000 times, something is seriously wrong
+	for version := 0; fileExist(resultFile) && version < 1000; version++ {
+		resultFile = filename + "." + strconv.Itoa(version)
+		if version >= 1000 {
+			return "", errors.New("file version >1000,create file failed")
+		}
+	}
+	return resultFile, nil
+
+}
+
+func IsContain(items []string, item string) bool {
+	for _, eachItem := range items {
+		if eachItem == item {
+			return true
+		}
+	}
+	return false
+}
+
+func IsDMLEvent(binlogEvent *replication.BinlogEvent) bool {
+	if binlogEvent.Header.EventType.String() == "WriteRowsEventV2" || binlogEvent.Header.EventType.String() == "DeleteRowsEventV2" || binlogEvent.Header.EventType.String() == "UpdateRowsEventV2" {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func DMLEvenType(e *replication.BinlogEvent) string {
+	if e.Header.EventType.String() == "DeleteRowsEventV2" {
+		return "DELETE"
+	} else if e.Header.EventType.String() == "WriteRowsEventV2" {
+		return "INSERT"
+	} else if e.Header.EventType.String() == "UpdateRowsEventV2" {
+		return "UPDATE"
+	}
+	return ""
+}
+
+func GetDBConnect(dsn string) (db1 *sql.DB, error error) {
+	db, err := sql.Open("mysql", dsn)
+	return db, err
+}
+
+func concatSqlFromBinlogEvent(args *Args, db *sql.DB, e *replication.BinlogEvent, row []interface{}, noPk bool, flashBack bool, eStartPos uint32, colsNames []string, colsNamesPrimary []string) (sql string, error error) {
+	//sql = concat_sql_from_binlog_event(cursor=cursor, binlog_event=binlog_event, no_pk=self.no_pk, row=row, flashback=self.flashback, e_start_pos=e_start_pos)
+	sql_concat := ""
+	//如果同时打开了回滚和没有主键则报错
+	if args.flashBack && args.noPK {
+		return sql_concat, errors.New("only one of flashback or no_pk can be True")
+	}
+
+	if !(e.Header.EventType.String() == "DeleteRowsEventV2" || e.Header.EventType.String() == "WriteRowsEventV2" || e.Header.EventType.String() == "UpdateRowsEventV2" || e.Header.EventType.String() == "QueryEvent") {
+		return sql_concat, errors.New("binlog_event must be WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent or QueryEvent")
+		return sql_concat, nil
+	}
+
+	if e.Header.EventType.String() == "DeleteRowsEventV2" || e.Header.EventType.String() == "WriteRowsEventV2" || e.Header.EventType.String() == "UpdateRowsEventV2" {
+		//fmt.Println(row)
+		sql_concat = generateSqlPattern(e, row, flashBack, *noPK, colsNames, colsNamesPrimary)
+		sql_time, _ := time.Parse("2006-01-02 15:04:05", time.Unix(int64(e.Header.Timestamp), 0).Format("2006-01-02 15:04:05"))
+		sql_concat = sql_concat + fmt.Sprintf(" # start %d end %d time %s", eStartPos, e.Header.LogPos, sql_time)
+		//fmt.Println("generateSqlPattern返回值"+sql_concat)
+		return sql_concat, nil
+
+	} else if args.flashBack == false && e.Header.EventType.String() == "QueryEvent" {
+		fmt.Println("---------------------------这是一个QueryEvent")
+		queryEvent, ok := e.Event.(*replication.QueryEvent)
+		if ok && string(queryEvent.Query) != "BEGIN" && string(queryEvent.Query) != "COMMIT" {
+			if queryEvent.Schema != nil || string(queryEvent.Schema) != "" {
+				sql_concat = fmt.Sprintf("USE %s;\n", queryEvent.Schema)
+			}
+			sql_concat := sql_concat + fmt.Sprintf("%s;", string(queryEvent.Query))
+			return sql_concat, nil
+		}
+	}
+	return "", nil
+}
+
+func concatUpdateSqlFromBinlogEvent(args *Args, db *sql.DB, e *replication.BinlogEvent, beforeValue []interface{}, afterValue []interface{}, noPk bool, flashBack bool, eStartPos uint32, colsNames []string, colsNamesPrimary []string) (sql string, error error) {
+	sql_concat := ""
+	if args.flashBack && args.noPK {
+		return sql_concat, errors.New("only one of flashback or no_pk can be True")
+	}
+
+	if !(e.Header.EventType.String() == "UpdateRowsEventV2") {
+		return sql_concat, errors.New("binlog_event must be UpdateRowsEvent")
+	}
+	if e.Header.EventType.String() == "UpdateRowsEventV2" {
+		sql_concat = generateUpdateSqlPattern(e, beforeValue, afterValue, flashBack, *noPK, colsNames, colsNamesPrimary)
+		//fmt.Println("generateUpdateSqlPattern返回值"+sql_concat)
+		return sql_concat, nil
+	}
+	return "", nil
+
+}
+
+func generateSqlPattern(e *replication.BinlogEvent, row []interface{}, flashBack bool, noPK bool, colsNames []string, colsNamesPrimary []string) string {
+	event, ok := e.Event.(*replication.RowsEvent)
+	if !ok {
+		fmt.Println("It's not ok for type RowsEvent")
+		return ""
+	}
+	if flashBack == true {
+		if e.Header.EventType.String() == "WriteRowsEventV2" {
+			tmpColsNames := compareDelItems(colsNames, row)
+			tmpColWheres := strings.Join(tmpColsNames, " AND ")
+			sql := fmt.Sprintf("DELETE FROM %s.%s WHERE %s limit 1;", event.Table.Schema, event.Table.Table, tmpColWheres)
+			//fmt.Println(sql)
+			return sql
+		} else if e.Header.EventType.String() == "DeleteRowsEventV2" {
+			var colsNamesDian []string
+			for _, col := range colsNames {
+				colsNamesDian = append(colsNamesDian, "`"+col+"`")
+			}
+			tmpColNames := strings.Join(colsNamesDian, ",")
+			tmpColsValues := compareInsertItems(colsNames, row)
+			tmpColValuesInsert := strings.Join(tmpColsValues, ",")
+			sql := fmt.Sprintf("INSERT INTO `%s`.`%s`(%s) VALUES (%s);", event.Table.Schema, event.Table.Table, tmpColNames, tmpColValuesInsert)
+			//fmt.Println(sql)
+			return sql
+		}
+	} else {
+		if e.Header.EventType.String() == "WriteRowsEventV2" {
+			var colsNamesDian []string
+			for _, col := range colsNames {
+				colsNamesDian = append(colsNamesDian, "`"+col+"`")
+			}
+			//if noPK{
+			//	if len(colsNamesPrimary)>0 || colsNamesPrimary !=nil{
+			//		var noPkcolsNamesArray []string
+			//		for _,col:= range(colsNames){
+			//			for _,colPrimary:=range(colsNamesPrimary){
+			//				if colPrimary!=col{
+			//					noPkcolsNamesArray=append(noPkcolsNamesArray,colPrimary)
+			//				}
+			//
+			//			}
+			//		}
+			//		colsNames=noPkcolsNamesArray
+			//	}
+			//}
+
+			tmpColNames := strings.Join(colsNamesDian, ",")
+			tmpColsValues := compareInsertItems(colsNames, row)
+			tmpColValuesInsert := strings.Join(tmpColsValues, ",")
+			fmt.Println(tmpColsValues)
+			sql := fmt.Sprintf("INSERT INTO `%s`.`%s`(%s) VALUES (%s);", event.Table.Schema, event.Table.Table, tmpColNames, tmpColValuesInsert)
+			//fmt.Println(sql)
+			return sql
+		} else if e.Header.EventType.String() == "DeleteRowsEventV2" {
+			tmpColsNames := compareDelItems(colsNames, row)
+			tmpColWheres := strings.Join(tmpColsNames, " AND ")
+			sql := fmt.Sprintf("delete from %s.%s where %s limit 1;", event.Table.Schema, event.Table.Table, tmpColWheres)
+			//fmt.Println(sql)
+			return sql
+		}
+	}
+	return ""
+}
+
+func generateUpdateSqlPattern(e *replication.BinlogEvent, beforeValue []interface{}, afterValue []interface{}, flashBack bool, noPK bool, colsNames []string, colsNamesPrimary []string) string {
+	event, ok := e.Event.(*replication.RowsEvent)
+	if !ok {
+		fmt.Println("It's not ok for type RowsEvent")
+		return ""
+	}
+	if flashBack == true {
+		if e.Header.EventType.String() == "UpdateRowsEventV2" {
+			tmpBeforeValues := compareSetUpdateItems(colsNames, beforeValue)
+			tmpAfterValues := compareWhereUpdateItems(colsNames, afterValue)
+			tmpSet := strings.Join(tmpBeforeValues, " AND ")
+			tmpWhere := strings.Join(tmpAfterValues, " AND ")
+			sql := fmt.Sprintf("UPDATE `%s`.`%s` SET %s WHERE %s LIMIT 1;", event.Table.Schema, event.Table.Table, tmpSet, tmpWhere)
+			//fmt.Println(sql)
+			return sql
+		}
+	} else {
+		if e.Header.EventType.String() == "UpdateRowsEventV2" {
+			tmpBeforeValues := compareWhereUpdateItems(colsNames, beforeValue)
+			tmpAfterValues := compareSetUpdateItems(colsNames, afterValue)
+			tmpWhere := strings.Join(tmpBeforeValues, " AND ")
+			tmpSet := strings.Join(tmpAfterValues, " AND ")
+			sql := fmt.Sprintf("UPDATE `%s`.`%s` SET %s WHERE %s LIMIT 1;", event.Table.Schema, event.Table.Table, tmpSet, tmpWhere)
+			//fmt.Println(sql)
+			return sql
+		}
+	}
+	return ""
+}
+
+func GetColsInfo(db *sql.DB, tableId string, tableSchema string, tableName string, colsMap map[string][]string) (colsMaps map[string][]string) {
+	col_sql := fmt.Sprintf("SELECT COLUMN_NAME,COLUMN_KEY FROM information_schema.columns WHERE table_schema = '%s' AND table_name = '%s';", tableSchema, tableName)
+	//fmt.Println(col_sql)
+	rows, err := db.Query(col_sql)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	var colsArray []string
+	var colsPrimary []string
+	for rows.Next() {
+		var colName string
+		var colPrimary string
+		rows.Scan(&colName, &colPrimary)
+		colsArray = append(colsArray, colName)
+		if colPrimary == "PRI" {
+			colsPrimary = append(colsPrimary, colName)
+		}
+	}
+	colsMap = make(map[string][]string)
+	m_key := tableId + "." + tableSchema + "." + tableName
+	m_key_primary := tableId + "." + tableSchema + "." + tableName + ".primary"
+	colsMap[m_key] = colsArray
+	colsMap[m_key_primary] = colsPrimary
+	//fmt.Println(colsMap)
+	return colsMap
+
+}
+
+func compareDelItems(colsNames []string, row []interface{}) []string {
+	var colsWheres []string
+	for num, col := range colsNames {
+		if row[num] == nil {
+			row[num] = "NULL"
+		}
+		//colsWhere:=fmt.Sprintf("%s=%v",col,row[num])
+		if _, ok := row[num].([]byte); ok {
+			colsWhere := fmt.Sprintf("`%s` = %q", col, row[num])
+			colsWheres = append(colsWheres, colsWhere)
+		} else {
+			if row[num] == "NULL" {
+				colsWhere := fmt.Sprintf("`%s` IS NULL ", col)
+				colsWheres = append(colsWheres, colsWhere)
+			} else {
+				colsWhere := fmt.Sprintf("`%s` = %#v", col, row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			}
+		}
+		//fmt.Println(colsWheres)
+	}
+	return colsWheres
+}
+
+func compareInsertItems(colsNames []string, row []interface{}) []string {
+	var colsWheres []string
+	for num, _ := range colsNames {
+		if row[num] == nil {
+			row[num] = "NULL"
+		}
+		//colsWhere:=fmt.Sprintf("%s=%v",col,row[num])
+		if _, ok := row[num].([]byte); ok {
+			colsWhere := fmt.Sprintf("%q", row[num])
+			colsWheres = append(colsWheres, colsWhere)
+		} else {
+			if row[num] == "NULL" {
+				colsWhere := fmt.Sprintf("%v", row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			} else {
+				colsWhere := fmt.Sprintf("%#v", row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			}
+
+		}
+		//fmt.Println(colsWheres)
+	}
+	return colsWheres
+}
+
+func compareSetUpdateItems(colsNames []string, row []interface{}) []string {
+	var colsWheres []string
+	for num, col := range colsNames {
+		if row[num] == nil {
+			row[num] = "NULL"
+		}
+		if _, ok := row[num].([]byte); ok {
+			colsWhere := fmt.Sprintf("`%s` = %q", col, row[num])
+			colsWheres = append(colsWheres, colsWhere)
+		} else {
+			if row[num] == "NULL" {
+				colsWhere := fmt.Sprintf("`%s` = %v", col, row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			} else {
+				colsWhere := fmt.Sprintf("`%s` =%#v", col, row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			}
+		}
+		//fmt.Println(colsWheres)
+	}
+	return colsWheres
+}
+
+func compareWhereUpdateItems(colsNames []string, row []interface{}) []string {
+	var colsWheres []string
+	for num, col := range colsNames {
+		if row[num] == nil {
+			row[num] = "NULL"
+		}
+		if _, ok := row[num].([]byte); ok {
+			colsWhere := fmt.Sprintf("`%s` =%q", col, row[num])
+			colsWheres = append(colsWheres, colsWhere)
+		} else {
+			if row[num] == "NULL" {
+				colsWhere := fmt.Sprintf("`%s` IS NULL ", col)
+				colsWheres = append(colsWheres, colsWhere)
+			} else {
+				colsWhere := fmt.Sprintf("`%s` =%#v", col, row[num])
+				colsWheres = append(colsWheres, colsWhere)
+			}
+		}
+		//fmt.Println(colsWheres)
+	}
+	return colsWheres
+}
---
+++ goflash/flashgo.go
@@ -0,0 +1,286 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/siddontang/go-mysql/mysql"
+	"github.com/siddontang/go-mysql/replication"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"io"
+	"os"
+	"reflect"
+	"runtime"
+	"time"
+)
+
+var Version = "0.0.1"
+
+var (
+	user         = kingpin.Flag("user", "MySQL Username to log in as").Short('u').Default("root").String()
+	host         = kingpin.Flag("host", "Host the MySQL database server located").Short('h').Default("127.0.0.1").String()
+	port         = kingpin.Flag("port", "MySQL port to use").Short('P').Uint16()
+	password     = kingpin.Flag("password", "MySQL Password to use").Short('p').Default("").String()
+	charset      = kingpin.Flag("charset", "mysql charset").Default("utf8").String()
+	startFile    = kingpin.Flag("start-file", "start binlog file name").Default("").String()
+	startPos     = kingpin.Flag("start-position", "start binlog position ").Uint32()
+	stopFile     = kingpin.Flag("stop-file", "end binlog file name").Default("").String()
+	stopPos      = kingpin.Flag("stop-position", "end binlog position ").Uint32()
+	startTime    = kingpin.Flag("start-datetime", "Start time. format %%Y-%%m-%%d %%H:%%M:%%S").Default("").String()
+	stopTime     = kingpin.Flag("stop-datetime", "Stop Time. format %%Y-%%m-%%d %%H:%%M:%%S;").Default("").String()
+	noPK         = kingpin.Flag("no-primary-key", "Generate insert sql without primary key if exists").Short('K').Bool()
+	flashBack    = kingpin.Flag("flashback", "Flashback data to start_position of start_file").Short('B').Bool()
+	stopNever    = kingpin.Flag("stop-never", "Continuously parse binlog. default: stop at the latest event when you start.").Bool()
+	backInterval = kingpin.Flag("back-interval", "Sleep time between chunks of 1000 rollback sql. set it to 0 if do not need sleep").Default("").String()
+	tables       = kingpin.Flag("tables", "tables you want to process").Short('t').Default("").String()
+	databases    = kingpin.Flag("databases", "dbs you want to process").Short('d').Default("").String()
+	onlyDML      = kingpin.Flag("only-dml", "only print dml, ignore ddl").Bool()
+	sqlType      = kingpin.Flag("sql-type", "Sql type you want to process, support INSERT, UPDATE, DELETE").Default("DELETE,UPDATE,INSERT").String()
+)
+
+func processBinlog(dsn *Dsn, args *Args) {
+	cfg := replication.BinlogSyncerConfig{
+		ServerID: 10010,
+		Flavor:   "mysql",
+		Host:     dsn.host,
+		Port:     dsn.port,
+		User:     dsn.user,
+		Password: dsn.password,
+	}
+	db, err := GetDBConnect(dsn.String())
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	flagLastEvent := false
+	syncer := replication.NewBinlogSyncer(cfg)
+	pos := mysql.Position{args.startFile, args.startPos}
+	stream, _ := syncer.StartSync(pos)
+	eStartPos, lastPos := args.startPos, args.startPos
+	fmt.Println(eStartPos, lastPos)
+	//创建文件存储数据
+	fileNameHeader := fmt.Sprintf("%s.%d", dsn.host, dsn.port)
+	tmpFile, err := createUniqueFile(fileNameHeader)
+	file, err := os.Create(tmpFile)
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println("创建临时文件--------" + tmpFile)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	//fmt.Println("args.stopNever--", args.stopNever)
+	//fmt.Println(args.binlogArray)
+	//对数据进行循环处理
+	colsMap := make(map[string][]string)
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		binlogEvent, err := stream.GetEvent(ctx)
+		//fmt.Println("----------------------------------------------------------------------")
+		//fmt.Println(binlogEvent.Header.EventType.String())
+		pos.Pos = binlogEvent.Header.LogPos
+		//fmt.Println("pos.Pos--", pos.Pos)
+		typeEvent := reflect.TypeOf(binlogEvent.Event).Elem()
+		evenTime, err := time.Parse("2006-01-02 15:04:05", time.Unix(int64(binlogEvent.Header.Timestamp), 0).Format("2006-01-02 15:04:05"))
+		//每个binlog头部和末尾会有RotateEvent event，我们取头部的RotateEvent来作为标记开始一个新的文件
+		if evenTime.Before(args.startTime) {
+			rotateEvent, ok := binlogEvent.Event.(*replication.RotateEvent)
+			if ok {
+				pos.Name = string(rotateEvent.NextLogName)
+			}
+		}
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		//确定当前的binlog file名称
+
+		//完成任务
+		if !(args.stopNever) {
+			//暂时没有加异常处理
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			//fmt.Println("event time is -----"+time.Unix(int64(binlogEvent.Header.Timestamp), 0).Format("2006-01-02 15:04:05"), "event binlog file is ------", pos.Name, "event binlog pos is ----- ", pos.Pos, binlogEvent.Header.LogPos)
+			if (pos.Name == args.stopFile && pos.Pos == args.stopPos && pos.Pos != 0) || (pos.Name == args.eofFile && pos.Pos == args.eofPos && pos.Pos != 0) {
+				fmt.Println(pos.Name, args.stopFile, pos.Pos, args.stopPos)
+				fmt.Println(pos.Name, args.eofFile, pos.Pos, args.eofPos)
+				fmt.Println("最后一个event是", pos.Name, pos.Pos)
+				flagLastEvent = true
+			} else if evenTime.Before(args.startTime) {
+				fmt.Println("当前event时间小于starttime")
+				if !((typeEvent.Name() == "RotateEvent") || (typeEvent.Name() == "FormatDescriptionEvent")) {
+					lastPos = pos.Pos
+				}
+				//continue
+			} else if (!IsContain(args.binlogArray, pos.Name)) || ((args.stopPos != 0) && (pos.Name == args.stopFile) && (pos.Pos > args.stopPos)) || (pos.Name == args.eofFile && pos.Pos > args.eofPos) || (evenTime.After(args.stopTime)) {
+				fmt.Println("已经到达末尾，终止输出")
+				break
+			}
+		}
+		//fmt.Println("已经是新的if啦")
+
+		queryEvent, ok := binlogEvent.Event.(*replication.QueryEvent)
+		if ok && string(queryEvent.Query) == "BEGIN" {
+			eStartPos = lastPos
+		}
+
+		queryEvent, ok = binlogEvent.Event.(*replication.QueryEvent)
+		if ok && !args.onlyDml {
+			//sql := "输出begin commit 等语句"
+			var row []interface{}
+			var colsNames []string
+			var colsNamesPrimary []string
+			sql, err := concatSqlFromBinlogEvent(args, db, binlogEvent, row, args.noPK, args.flashBack, eStartPos, colsNames, colsNamesPrimary)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			//fmt.Println("concat......" + sql)
+			if sql != "" {
+				//fmt.Println(sql)
+				file.WriteString(sql + "\n")
+			}
+		} else if IsDMLEvent(binlogEvent) && IsContain(args.sqlType, DMLEvenType(binlogEvent)) {
+
+			event, ok := binlogEvent.Event.(*replication.RowsEvent)
+			if !ok {
+				fmt.Println("It's not ok for type RowsEvent")
+				return
+			}
+			tableName := string(event.Table.Table)
+			schemaName := string(event.Table.Schema)
+			tableIdString := fmt.Sprintf("%v", event.TableID)
+			tableNameKey := tableIdString + "." + schemaName + "." + tableName
+			tableNamePrimaryKey := tableIdString + "." + schemaName + "." + tableName + ".primary"
+			if colsMap[tableNameKey] == nil {
+				//fmt.Println("表ID不存在于colsMap，进入数据库查询")
+				colsMap = GetColsInfo(db, tableIdString, schemaName, tableName, colsMap)
+			}
+			//fmt.Print("获取表字段名称为----")
+			//fmt.Println(colsMap)
+			//fmt.Println("rows event的库名和表名字是-----",tableName,schemaName)
+			colsNames := colsMap[tableNameKey]
+			colsNamesPrimary := colsMap[tableNamePrimaryKey]
+			count := 0
+
+			if (args.databases != nil && args.tables != nil && IsContain(args.databases, schemaName) && IsContain(args.tables, tableName)) || (args.databases == nil && args.tables != nil && IsContain(args.tables, tableName)) || (args.databases != nil && args.tables == nil && IsContain(args.databases, schemaName)) || (args.databases == nil && args.tables == nil) {
+
+				if binlogEvent.Header.EventType.String() == "UpdateRowsEventV2" {
+					var updateBeforeValue []interface{}
+					var updateAfterValue []interface{}
+					for _, row := range event.Rows {
+						if count%2 == 0 {
+							updateBeforeValue = row
+						} else {
+							updateAfterValue = row
+							sql, err := concatUpdateSqlFromBinlogEvent(args, db, binlogEvent, updateBeforeValue, updateAfterValue, args.noPK, args.flashBack, eStartPos, colsNames, colsNamesPrimary)
+							//fmt.Println(sql)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							if args.flashBack {
+								//fmt.Println(sql)
+								file.WriteString(sql + "\n")
+							} else {
+								//fmt.Println(sql)
+								file.WriteString(sql + "\n")
+							}
+						}
+						count++
+					}
+
+				} else {
+					for _, row := range event.Rows {
+						sql, err := concatSqlFromBinlogEvent(args, db, binlogEvent, row, args.noPK, args.flashBack, eStartPos, colsNames, colsNamesPrimary)
+						//fmt.Println(sql)
+						if err != nil {
+							fmt.Println(err.Error())
+						}
+						if args.flashBack {
+							file.WriteString(sql + "\n")
+							//fmt.Println(sql)
+						} else {
+							//fmt.Println(sql)
+							file.WriteString(sql + "\n")
+						}
+					}
+				}
+			}
+			//else{
+			//	fmt.Println("database or tables dont exist")}
+			//
+			////fmt.Println("打印特定的DML语句")
+		}
+
+		if !(typeEvent.Name() == "RotateEvent" || typeEvent.Name() == "FormatDescriptionEvent") {
+			lastPos = pos.Pos
+		}
+
+		if flagLastEvent {
+			break
+		}
+
+		cancel()
+		if err == context.DeadlineExceeded {
+			continue
+		}
+
+		//binlogEvent.Dump(os.Stdout)
+	}
+
+	if args.flashBack {
+		//fmt.Print("打印闪回sql语句\n")
+		rd, err := NewReadLineFromEnd(tmpFile)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		defer rd.Close()
+		file, err := os.Create(tmpFile + ".bak")
+		defer file.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		for {
+			data, err := rd.ReadLine()
+			file.WriteString(string(data))
+			if err != nil {
+				if err != io.EOF {
+				}
+				break
+			}
+			//fmt.Print(string(data))
+
+		}
+
+		//fmt.Println("----------------------------------------------------------------------")
+	}
+}
+
+func main() {
+	kingpin.Version(fmt.Sprintf("flashgo %s (built with %s)\n", Version, runtime.Version()))
+	kingpin.Parse()
+	mysqlStruct, _ := NewDsn(*user, *password, *host, *port, *charset)
+	dsn_string := mysqlStruct.String()
+	//fmt.Println(dsn_string)
+	//fmt.Printf("%+v", mysqlStruct)
+	//fmt.Println()
+	mysqlArgs, err := NewArgs(dsn_string, *startFile, *startPos, *stopFile, *stopPos, *startTime, *stopTime, *noPK, *flashBack, *stopNever, *backInterval, *onlyDML, *sqlType, *tables, *databases)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if mysqlArgs == nil {
+		return
+	}
+
+	//fmt.Printf("打印参数列表-------%+v", mysqlArgs)
+	//fmt.Println()
+	//fmt.Println(mysqlArgs.startTime.Format("2006-01-02 15:04:05"))
+	//fmt.Println(mysqlArgs.stopTime.Format("2006-01-02 15:04:05"))
+
+	//执行解析binlog
+	processBinlog(mysqlStruct, mysqlArgs)
+
+}
